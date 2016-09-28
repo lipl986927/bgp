73a5abe55a276dc2231570e1a3c43532b17313ee
sudks <sudarshana.ks@gmail.com>
2016-09-28 16:40:10 +0530

    Commit of setup.py and gitignore file

--- setup.py
+++ setup.py
@@ -7,11 +7,10 @@ scapy_package = 'scapy-python3' if sys.version_info.major==3 else 'scapy'
 
 setup(name="Bgp",
       version=VERSION,
-      description="A python stack to test bgp",
+      description="test tool bgp",
       author="Sudarshana K S",
       author_email='sudarshana.ks@gmail.com',
       platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
-      license="BSD",
       url="http://github.com/sudks/bgp",
       packages=find_packages(),
       install_requires=[scapy_package],
